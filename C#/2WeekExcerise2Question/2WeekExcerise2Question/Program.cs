a279906aedd0898e35bfc8303ef3a6ced2263585
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-09-15 19:08:56 +0200

    Adding my first if else code

---
+++ C#/2WeekExcerise2Question/2WeekExcerise2Question/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _2WeekExcerise2Question
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //…frågar om AIK (eller annat valfritt lag) är Sveriges bästa lag. Om användaren svarar ja skriver programmet ut ”Helt rätt!”, annars skrivs det ut ”Väldigt fel!”.
+
+            Console.WriteLine("Är AIK bästa lag i Sverige: ja/nej ");
+            string choice = Console.ReadLine();
+
+            if (choice == "ja")
+            {
+                Console.WriteLine("Helt rätt!");
+            }
+            else
+            {
+                Console.WriteLine("Väldigt fel!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
