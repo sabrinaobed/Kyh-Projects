0dd605ec4cc29663cf3d5320e9d6fe8e8bf94eff
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-09-15 18:51:46 +0200

    adding all changes

---
+++ C#/vecka 1/Excerise2MadLibs/Excerise2MadLibs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Excerise2MadLibs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*◦ Ett sällskapsspel som bygger på att spelarna får hitta på olika ord, utan att veta hurhistorien ser ut. Det liknar övningen med Sagan.
+                   ◦ Du skapar en historia med luckor.
+                   ◦ Spelaren får hitta på t ex verb, substantiv, adjektiv m m, utan att veta hur de kommer användas.
+                   ◦ När orden är ifyllda så visas historien, och förhoppningsvis uppstår en del komiska ordval.*/
+
+            Console.WriteLine("Skriv ord och skapa en Saga själv");
+
+            Console.WriteLine("Skriv en substantiv : flicka/pöjke ");
+            string noun = Console.ReadLine();
+
+            Console.WriteLine("Skriv en substantiv namn: ");
+            string nounName = Console.ReadLine();
+
+            Console.WriteLine("Skriv en substantiv plats : ");
+            string nounPlace = Console.ReadLine();
+
+            Console.WriteLine("Skriv en substantiv sak: ");
+            string nounThing = Console.ReadLine();
+
+            Console.WriteLine("Skriv en pronomen: hon/hon/hen ");
+            string pronoun = Console.ReadLine();
+
+            Console.WriteLine("Skriv en verb :");
+            string verb = Console.ReadLine();
+
+            Console.WriteLine("Skriv en adjektiv: ");
+            string adjective = Console.ReadLine();
+
+            Console.WriteLine($"Det var en gång i tiden ett {noun} som var väldigt {adjective}.{pronoun} hette {nounName}.{pronoun} bodde i {nounPlace}. {nounName} fungerade {adjective} till {verb} en {nounThing}");
+            Console.ReadLine();
+
+
+        
+        }
+
+
+    }
+}
---
+++ C#/vecka 2/Execerise4ConsoleColor/Execerise4ConsoleColor/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Execerise4ConsoleColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Windows-konsollen ser normal lite tråkig ut, men det går att göra en del saker.
+            // Titta på Console Class(System) | Microsoft Doc
+            //◦ Ändra properties:
+            //◦ T ex bakgrundsfärgen till Röd
+            //◦ Ändra förgrundsfärgen till Svart
+
+
+            //Ändra bakgrundsfärg till röd och förgrundsfärg till svart
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //Rensa konsolen och applicera ny färg
+            Console.Clear();
+            Console.WriteLine("Text med röd bakgrundsfärg och svart förgrundsfärg");
+
+            //Återställ färgen till standaradinställningarna(default settings)
+            Console.ResetColor();
+
+
+            Console.ReadLine();
+        }
+    }
+}
