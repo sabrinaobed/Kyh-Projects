ee728a65ab4e857534fda67ee11315d7ef7cc7e0
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2024-09-16 20:53:01 +0200

    if else temperature excerise

---
+++ C#/2WeekExcerise3Temperature/2WeekExcerise3Temperature/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2WeekExcerise3Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //frågar efter temperaturen i Svedala och Jukkasjärvi. Programmet berättar sedan var det är kallast.
+            Console.WriteLine("Vad är temperaturen i Svedala? ");
+            string tempSvedala = Console.ReadLine();
+            decimal tempSvedalaD = decimal.Parse(tempSvedala);
+
+            Console.WriteLine("Vad är tempaturen i Jukkasjärvi? ");
+            string tempJukkasjärvi = Console.ReadLine();
+            decimal tempJukkasjärviD = decimal.Parse(tempJukkasjärvi);
+
+            if (tempSvedalaD > tempJukkasjärviD)
+            {
+                Console.WriteLine("Jukkasjärvi är kallast");
+            }
+            else
+            {
+                Console.WriteLine("Svedala är kallast");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
